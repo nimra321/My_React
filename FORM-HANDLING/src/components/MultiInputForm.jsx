8a3ea84860e15157293cf9f3040faf61ea7af93f
nimra321 <nimrajahangir421@gmail.com>
2026-02-16 18:44:52 +0500

    Simple Form submission through useState

---
+++ FORM-HANDLING/src/components/MultiInputForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MultiInputForm() {
+  return (
+    <div>
+        <form>
+            <h1>Multiple Input Form</h1>
+        </form>
+    </div>
+  )
+}
+
+export default MultiInputForm
